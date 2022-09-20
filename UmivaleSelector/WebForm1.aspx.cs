5f0fd3e68c6123271a84ceb55914ea8fcd5ccf35
unknown <ferrallas@gmail.com>
2022-09-20 12:40:18 +0200

    Cambios consulta

--- UmivaleSelector/WebForm1.aspx.cs
+++ UmivaleSelector/WebForm1.aspx.cs
@@ -25,7 +25,7 @@ namespace UmivaleSelector
             {
                 string connectionString = Properties.Settings.Default.connectionString;
                 using (SqlConnection connection = new SqlConnection(connectionString))
-                using (SqlCommand command = new SqlCommand("select TOP 1 DNI,Centro,Call_ID from DNI where Caller = " + tel + " Order By FechaHora DESC", connection))
+                using (SqlCommand command = new SqlCommand("select TOP 1 ISNULL(DNI,'') as DNI, ISNULL(Centro,'') as Centro, ISNULL(Call_ID,'') as Call_ID FROM DNI WHERE Caller = " + tel + " Order By FechaHora DESC", connection))
                 {
                     connection.Open();
                     using (SqlDataReader reader = command.ExecuteReader())
