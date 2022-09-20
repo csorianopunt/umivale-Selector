4d12549d9b8b706b82bb6129e7381be5bbcf0c35
unknown <ferrallas@gmail.com>
2022-09-20 11:56:34 +0200

    [First Commit]

---
+++ UmivaleSelector/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="UmivaleSelector.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
+<script language="javascript" type="text/javascript">
+    $(document).ready(function () {
+		    window.parent.open(<%=url%>,"_self");
+        });
+</script>
---
+++ UmivaleSelector/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace UmivaleSelector
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public static string url;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //ej.: tel = 888802675022247 & callid = 85269
+
+            string tel = Request["tel"];
+            string callid = Request["callid"];
+
+            string connectionString = Properties.Settings.Default.connectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand("select TOP 1 DNI,Centro,Call_ID from DNI where Caller = " + tel + " Order By FechaHora DESC", connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            string DNI = reader["DNI"].ToString();
+                            string Centro = reader["Centro"].ToString();
+                            string CallID = reader["Call_ID"].ToString();
+
+                            /* si es SAC */
+                            if (Centro == Properties.Settings.Default.Centro)
+                            {
+                                url = "http://srvsynergy/synergy/docs/Pntv2preinputs.aspx?tel=" + tel + "&callid=" + callid;
+                            }
+                            /* si es Callcenter */
+                            else
+                            {
+                                url = "http://srvccsynergy/synergy/docs/Pntv2preinputs.aspx?tel=" + tel + "&callid=" + callid;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            url = "http://srvccsynergy/synergy/docs/Pntv2preinputsUni.aspx?tel=" + tel + "&callid=" + callid;
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
