2ec5cc61b6dd5e6cf9796313c433504421afb1f6
unknown <ferrallas@gmail.com>
2022-09-20 12:15:44 +0200

    Cambios consulta

--- UmivaleSelector/WebForm1.aspx.cs
+++ UmivaleSelector/WebForm1.aspx.cs
@@ -21,40 +21,41 @@ namespace UmivaleSelector
             string tel = Request["tel"];
             string callid = Request["callid"];
 
-            string connectionString = Properties.Settings.Default.connectionString;
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            using (SqlCommand command = new SqlCommand("select TOP 1 DNI,Centro,Call_ID from DNI where Caller = " + tel + " Order By FechaHora DESC", connection))
+            try
             {
-                connection.Open();
-                using (SqlDataReader reader = command.ExecuteReader())
+                string connectionString = Properties.Settings.Default.connectionString;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlCommand command = new SqlCommand("select TOP 1 DNI,Centro,Call_ID from DNI where Caller = " + tel + " Order By FechaHora DESC", connection))
                 {
-                    while (reader.Read())
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        try
+                        while (reader.Read())
                         {
-                            string DNI = reader["DNI"].ToString();
-                            string Centro = reader["Centro"].ToString();
-                            string CallID = reader["Call_ID"].ToString();
+                        
+                                string DNI = reader["DNI"].ToString();
+                                string Centro = reader["Centro"].ToString();
+                                string CallID = reader["Call_ID"].ToString();
 
-                            /* si es SAC */
-                            if (Centro == Properties.Settings.Default.Centro)
-                            {
-                                url = "http://srvsynergy/synergy/docs/Pntv2preinputs.aspx?tel=" + tel + "&callid=" + callid;
-                            }
-                            /* si es Callcenter */
-                            else
-                            {
-                                url = "http://srvccsynergy/synergy/docs/Pntv2preinputs.aspx?tel=" + tel + "&callid=" + callid;
-                            }
+                                /* si es SAC */
+                                if (Centro == Properties.Settings.Default.Centro)
+                                {
+                                    url = "http://srvsynergy/synergy/docs/Pntv2preinputs.aspx?tel=" + tel + "&callid=" + callid;
+                                }
+                                /* si es Callcenter */
+                                else
+                                {
+                                    url = "http://srvccsynergy/synergy/docs/Pntv2preinputs.aspx?tel=" + tel + "&callid=" + callid;
+                                }
                         }
-                        catch (Exception ex)
-                        {
-                            url = "http://srvccsynergy/synergy/docs/Pntv2preinputsUni.aspx?tel=" + tel + "&callid=" + callid;
-                        }
-
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                url = "http://srvccsynergy/synergy/docs/Pntv2preinputsUni.aspx?tel=" + tel + "&callid=" + callid;
+            }
+
 
         }
     }
